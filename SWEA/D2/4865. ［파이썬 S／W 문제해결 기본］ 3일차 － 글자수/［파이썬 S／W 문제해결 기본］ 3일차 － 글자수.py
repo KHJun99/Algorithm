6be73e7b8c37e2259b08b2be33dd10661f717f89
KHJune99 <153348854+KHJune99@users.noreply.github.com>
2025-07-31 01:10:05 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 글자수, Time: 59 ms, Memory: 51,712 KB -BaekjoonHub

---
+++ SWEA/D2/4865. ［파이썬 S／W 문제해결 기본］ 3일차 － 글자수/［파이썬 S／W 문제해결 기본］ 3일차 － 글자수.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+# 테스트 케이스 만큼 반복
+for i in range(1, t + 1):
+    str1 = input()
+    str2 = input()
+    
+    # 글자 개수 리스트
+    count_lst = list()
+    
+    # str1 알파벳 비교
+    for j in str1:
+        # 개수 카운트
+        match = str2.count(j)
+        count_lst.append(match)
+    
+    print(f'#{i} {max(count_lst)}')    
