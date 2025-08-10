9a2a38cf91c66616e8416dff8d4a5a07ba54b9f8
KHJune99 <153348854+KHJune99@users.noreply.github.com>
2025-08-10 23:39:09 +0900

    [D1] Title: 연속한 1의 개수, Time: 70 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
+++ SWEA/D1/9386. 연속한 1의 개수/연속한 1의 개수.py
@@ -1,19 +1,19 @@
-
 T = int(input())
 
-for case in range(1, T + 1):
+for tc in range(1, T + 1):
     N = int(input())
-    array = list(map(int, input().strip()))  # 공백 없이 붙어있으니 split() 대신 그대로 변환
+    arr = list(map(int, input()))
+    stack = []
+    count = 0
+    max_count = 0
 
-    max_count = 0   # 연속된 1의 최대 길이
-    current_count = 0  # 현재 연속된 1의 길이
-
-    for num in array:
-        if num == 1:
-            current_count += 1
-            if current_count > max_count:
-                max_count = current_count
-        else:
-            current_count = 0  # 0을 만나면 연속 끊김
-
-    print(f'#{case} {max_count}')
+    for i in range(len(arr)):
+        if arr[i] == 1:
+            stack.append(arr[i])
+            count += 1
+            if max_count < count:
+                max_count = count
+        if stack and arr[i] == 0:
+            stack.pop()
+            count = 0
+    print(f'#{tc} {max_count}')
