b5ef1713d8ee3242bfd8a1185116133135715e16
KHJune99 <153348854+KHJune99@users.noreply.github.com>
2025-08-10 21:19:55 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 83 ms, Memory: 60,800 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,27 +1,27 @@
-def my_max(*lst):
-    list_max = lst[0]
-    for num in lst[1:]:
-        if num > list_max:
-            list_max = num
-    return list_max
+def my_max(lst):
+    max_val = float('-inf')
+    for i in lst:
+        if max_val < i:
+            max_val = i
+    return max_val
 
 
 for tc in range(1, 11):
     N = int(input())
-    buildings = list(map(int, input().split()))
-    total_view = 0
+    building = list(map(int, input().split()))
+    room = 0
 
-    # 2번째 건물부터 N-3번째까지 탐색
     for i in range(2, N - 2):
-        current = buildings[i]
-        left2 = buildings[i - 2]
-        left1 = buildings[i - 1]
-        right1 = buildings[i + 1]
-        right2 = buildings[i + 2]
+        left_build_1 = building[i - 1]
+        left_build_2 = building[i - 2]
+        right_build_1 = building[i + 1]
+        right_build_2 = building[i + 2]
 
-        max_neighbor = my_max(left2, left1, right1, right2)
+        height = [left_build_1, left_build_2, right_build_1, right_build_2]
+        max_height = my_max(height)
 
-        if current > max_neighbor:
-            total_view += (current - max_neighbor)
+        diff = building[i] - max_height
+        if diff > 0:
+            room += diff
 
-    print(f"#{tc} {total_view}")
+    print(f'#{tc} {room}')
