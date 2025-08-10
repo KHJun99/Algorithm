0719ff0807eff6ec939ae84d91e9ec27fefba1ae
KHJune99 <153348854+KHJune99@users.noreply.github.com>
2025-08-10 22:16:31 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 구간합, Time: 73 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
+++ SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
@@ -1,23 +1,36 @@
+def my_min(lst):
+    min_val = float('inf')
+    for i in lst:
+        if min_val > i:
+            min_val = i
+    return min_val
+
+
+def my_max(lst):
+    max_val = float('-inf')
+    for i in lst:
+        if max_val < i:
+            max_val = i
+    return max_val
+
+
+def my_sum(lst):
+    total = 0
+    for i in lst:
+        total += i
+    return total
+
+
 T = int(input())
-
-for case in range(1, T + 1):
+total = []
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    ai = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
 
-    # 구간합 리스트 생성
-    prefix_sum = []
     for i in range(N - M + 1):
-        prefix_sum.append(sum(ai[i:i+M]))
+        lst_sum = my_sum(arr[i:i+M])
+        total.append(lst_sum)
 
-    # 최대, 최소 찾기
-    max_value = prefix_sum[0]
-    min_value = prefix_sum[0]
-
-    for val in prefix_sum:
-        if val > max_value:
-            max_value = val
-        if val < min_value:
-            min_value = val
-
-    result = max_value - min_value
-    print(f'#{case} {result}')
+    result = my_max(total) - my_min(total)
+    print(f'#{tc} {result}')
+    total.clear()
