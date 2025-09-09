b0d3d4c60a129629fdc498ead6b9873a07d173ad
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-09-09 18:00:02 +0900

    [Silver II] Title: 로또, Time: 124 ms, Memory: 111488 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,19 @@
+def subset(cnt, start):
+    if cnt == 6:
+        print(*q)
+        return
+
+    for i in range(start, k):
+        q.append(lst[i])
+        subset(cnt+1, i+1)
+        q.pop()
+
+while True:
+    lst = list(map(int, input().split()))
+    k = lst.pop(0)
+    if k == 0:
+        break
+
+    q = []
+    subset(0, 0)
+    print()
