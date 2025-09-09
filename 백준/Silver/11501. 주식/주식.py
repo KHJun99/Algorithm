d3986e7450db44ae5fcdab7b531cc4e472f46b36
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-09-10 00:00:10 +0900

    [Silver II] Title: 주식, Time: 1232 ms, Memory: 320000 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,12 +1,16 @@
+# 이익을 구하는 함수
 def stock(lst):
     global N
+    # lst의 마지막 인덱스를 최대값으로 가정
     max_price = lst[N - 1]
     price = 0
 
     for idx in range(N - 1, -1, -1):
+        # max_price보다 리스트값이 작으면 이익 발생
         if max_price > lst[idx]:
             diff = max_price - lst[idx]
             price += diff
+        # 이익이 발생할 수 없으므로 최대값 갱신
         else:
             max_price = lst[idx]
 
