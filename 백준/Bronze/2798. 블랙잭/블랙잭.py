5ebce6440aeec77b6176fc2fe8ae540ce6d24fcb
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-09-12 01:57:54 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,22 @@
-n, m = map(int, input().split())
+# 백준_2798_블랙잭 (B2)
+"""
+문제
+- 플레이어가 고른 카드(3장) 의 합은 M을 넘지 않으면서 M과 최대한 가까운 수를 출력하시오.
+"""
+import sys
+
+N, M = map(int, input().split())
 card = list(map(int, input().split()))
-max_sum = 0
 
-# 카드 리스트를 정렬합니다.
-card.sort()
+best = 0
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            result = card[i] + card[j] + card[k]
+            if result == M:
+                print(M)
+                sys.exit(0)         # 정확히 맞추면 종료
+            if result < M and result > best:
+                best = result
 
-# 3개의 카드 조합을 찾기 위한 중첩 루프
-for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            current_sum = card[i] + card[j] + card[k]
-            if current_sum <= m and current_sum > max_sum:
-                max_sum = current_sum
-
-print(max_sum)
+print(best)
