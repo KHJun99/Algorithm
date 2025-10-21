03da361ec39e08b74495613763208b010054ebad
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-10-21 17:50:55 +0900

    [Silver V] Title: 종이자르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,26 +1,38 @@
-# 종이 자르기
-width, length = map(int, input().split())
-T = int(input())
+g, s = map(int, input().split())
 
+cnt = int(input())
 
-# 가로(수평) 절단선: 세로 길이 방향 좌표(0과 length 포함)
-h = [0, length]
-# 세로(수직) 절단선: 가로 길이 방향 좌표(0과 width 포함)
-v = [0, width]
+garo = [0, g]
+sero = [0, s]
 
-for _ in range(T):
-    a, b = map(int, input().split())
-    if a == 0:      # 가로(수평)로 자르기 → 세로 방향 좌표 리스트(h)에 추가
-        h.append(b)
-    else:           # 세로(수직)로 자르기 → 가로 방향 좌표 리스트(v)에 추가
-        v.append(b)
+for _ in range(cnt):
+    dir, idx = map(int, input().split())
 
-h.sort()
-v.sort()
+    if dir == 0:
+        sero.append(idx)
+    else:
+        garo.append(idx)
 
+garo.sort()
+sero.sort()
 
-# 인접 절단선 사이의 최대 간격(=조각의 최대 높이/너비)
-max_h = max(h[i + 1] - h[i] for i in range(len(h) - 1))
-max_w = max(v[i + 1] - v[i] for i in range(len(v) - 1))
+max_g = 0
+i = 0
+while i < len(garo) - 1:
+    width = garo[i + 1] - garo[i]
+    if width > max_g:
+        max_g = width
 
-print(max_h * max_w)
+    i += 1
+
+max_s = 0
+j = 0
+while j < len(sero) - 1:
+    height = sero[j + 1] - sero[j]
+    if height > max_s:
+        max_s = height
+
+    j += 1
+
+print(max_g * max_s)
+
