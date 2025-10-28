4b0ef7223e8dc853426026fa83c8854fef7ab464
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-10-28 16:15:09 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 4.99 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -3,17 +3,24 @@ def solution(board, moves):
     n = len(board)
     
     stack = []
-
+    
+    # moves 순서대로 진행
     for idx in moves:
+        # 행을 순환하면서 인형이 있으면 stack에 넣는 과정
         for r in range(n):
+            # stack의 -1번 인덱스와 -2번 인덱스가 같으면 둘다 pop() 후
+            # answer += 2
             if len(stack) >= 2 and stack[-1] == stack[-2]:
                 stack.pop()
                 stack.pop()
                 answer += 2
-                
+            
+            # 인형이 있으면 스택에 인형을 삽입
             if board[r][idx - 1] != 0:
                 stack.append(board[r][idx - 1])
+                # 인형을 기존 위치에서 뽑았기 때문에 0으로 초기화
                 board[r][idx - 1] = 0
+                # break를 하지 않으면 모든 열의 인형을 다 뽑기 때문에 필수
                 break
             
     return answer
