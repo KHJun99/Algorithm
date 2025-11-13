2412e8273f83887e19bf3400f3f5edae7f15cca8
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-11-13 17:03:27 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,17 +1,24 @@
-# 벌써 만났습니다.
-import sys
+def combination(n, r):
+    dp = [[0] * (r + 1) for _ in range(n + 1)]
 
-input = sys.stdin.readline
+    # 초기값 : nC0 = 1
+    for i in range(n + 1):
+        dp[i][0] = 1
 
-def factorial(n):
-    num = 1
+    # 파스칼 삼각형 : dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
     for i in range(1, n + 1):
-        num *= i
-    return num
+        for j in range(1, min(i, r) + 1):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
 
-test = int(input())
+    return dp[n][r]
 
-for _ in range(test):
-    n, m = map(int, input().split())
-    bridge = factorial(m) // (factorial(n) * factorial(m - n))
-    print(bridge)
+
+T = int(input())
+result = []
+for _ in range(T):
+    N, M = map(int, input().split())
+
+    result.append(combination(M, N))
+
+for row in result:
+    print(row)
