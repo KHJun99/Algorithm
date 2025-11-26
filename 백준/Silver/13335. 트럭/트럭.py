5c3e1a49e1bf649a58b722cde6b473694e83ccdf
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-11-27 02:04:00 +0900

    [Silver I] Title: 트럭, Time: 64 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,33 +1,36 @@
 from collections import deque
 
-
-def moving():
-    bridge = deque([0] * w)  # 다리를 w 길이의 큐로 초기화 (0은 빈 공간)
-    bridge_w = 0  # 현재 다리 위의 총 무게
-    cnt = 0  # 경과 시간
-    indx = 0  # 다음에 올라갈 트럭의 인덱스
-
-    while indx < n or bridge_w > 0:  # 모든 트럭이 올라가고 다리가 비워질 때까지
-        cnt += 1
-
-        # 다리에서 트럭 하나 내보내기
-        exited = bridge.popleft()
-        bridge_w -= exited
-
-        # 새 트럭을 다리에 올릴 수 있는지 확인
-        if indx < n:
-            if bridge_w + truck_w[indx] <= L:  # 무게 제한 확인
-                bridge.append(truck_w[indx])
-                bridge_w += truck_w[indx]
-                indx += 1
-            else:
-                bridge.append(0)  # 트럭을 올릴 수 없으면 빈 공간 추가
-
-    return cnt
-
-
+# n : 다리르 건너는 트럭의 수, w : 다리의 길이, l : 다리의 최대하중
 n, w, L = map(int, input().split())
-truck_w = list(map(int, input().split()))
+trucks = list(map(int, input().split()))     # 트럭의 무게
 
-final_result = moving()
-print(final_result)
+bridge = deque([0] * w)     # 다리
+
+current_weight = 0      # 현재 다리 위의 무게
+time = 0                # 경과 시간
+
+waiting = deque(trucks)     # 대기 중인 트럭
+
+# 모든 트럭이 다리를 건널 때까지 반복
+while waiting or current_weight > 0:
+    time += 1
+
+    # 다리의 맨 앞 트럭(없으면 빈 공간) 제거
+    out = bridge.popleft()
+    current_weight -= out
+
+    if waiting:
+        # 무게 조건 확인
+        if current_weight + waiting[0] <= L:
+            # 트럭 진입
+            truck = waiting.popleft()
+            bridge.append(truck)
+            current_weight += truck
+        else:
+            # 진입 불가 - 빈 공간 추가
+            bridge.append(0)
+    else:
+        # 대기 트럭이 없으면 빈 공간 추가
+        bridge.append(0)
+
+print(time)
