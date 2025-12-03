fd33a4fb3ba241311576e27b27ecf05c4ffe325d
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2025-12-03 17:55:01 +0900

    [Silver I] Title: 부등호, Time: 116 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -0,0 +1,38 @@
+def check(a, b, sign):
+    if sign == '<':
+        return a < b
+    else:
+        return a > b
+
+
+def make_num(depth, num_str):
+    if depth == k + 1:
+        results.append(num_str)
+        return
+
+    for num in range(10):
+        if visited[num]:
+            continue
+
+        if depth > 0:
+            prev_num = int(num_str[-1])
+            curr_sign = signs[depth - 1]
+
+            if not check(prev_num, num, curr_sign):
+                continue
+
+        visited[num] = True
+        make_num(depth + 1, num_str + str(num))
+        visited[num] = False
+
+k = int(input())
+signs = input().split()
+visited = [False] * 10
+results = []
+
+make_num(0, "")
+
+results.sort()
+
+print(results[-1])
+print(results[0])
