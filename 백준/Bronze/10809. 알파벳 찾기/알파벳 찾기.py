475685255bb153bfb921d81e788ea3debf0317be
KHJun99 <153348854+KHJun99@users.noreply.github.com>
2026-02-18 03:12:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,11 +1,5 @@
-S = str(input())
+word = input()
+alphabet = [chr(ord('a') + i) for i in range(26)]
 
-S_list = list(map(str, str(S)))
-
-alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-for i in range(len(alphabet_list)):
-    if alphabet_list[i] in S_list:
-        print(S_list.index(alphabet_list[i]), end = " ")
-    else:
-        print("-1", end = " ")
+result = [word.find(alpha) for alpha in alphabet]
+print(*result)
